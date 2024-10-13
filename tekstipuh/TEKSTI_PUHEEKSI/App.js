7425938d21a69a8f12a13e87b527368342b6d952
AbuAk1 <bgh553@myy.haaga-helia.fi>
2024-10-13 14:58:34 +0300

    Done ex 14

--- tekstipuh/TEKSTI_PUHEEKSI/App.js
+++ tekstipuh/TEKSTI_PUHEEKSI/App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, onChangeText] = useState("Write something to hear!");
+
+  const speak = () => {
+    const thingToSay = text;
+    Speech.speak(thingToSay);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+      <Button title="Press to hear text" onPress={speak} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +35,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+  
+    height: 50,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
